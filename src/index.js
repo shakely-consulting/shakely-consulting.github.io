da7990c210dff26b9a2837826bc4f5c71d737fcb
messidagod <joeshakely@gmail.com>
2020-04-05 18:45:57 -0700

    idk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
-// import Home from "./components/Home";
 import Navigation from "./components/Navigation";
 
 const Main = () => (
